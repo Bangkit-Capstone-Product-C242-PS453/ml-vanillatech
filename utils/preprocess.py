1ff5175909b301524fb7f28af95cd8eb999fd450
NazarAF <52284923+NazarAF@users.noreply.github.com>
2024-12-12 12:58:37 +0800

    Update Classes and Actions

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,11 +1,6 @@
 import tensorflow as tf
 
 def preprocess_image(image_stream):
-    """
-    Preprocessing gambar sesuai spesifikasi.
-    :param image_stream: Stream file gambar (BytesIO).
-    :return: Tensor siap digunakan model.
-    """
     image_bytes = image_stream.read()
     img = tf.image.decode_image(image_bytes, channels=3)
     img = tf.image.resize(img, [256, 256])
