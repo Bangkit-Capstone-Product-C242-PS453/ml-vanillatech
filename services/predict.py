b6c08f46893e343ebbf1bb33d3bd636c89f0380c
NazarAF <52284923+NazarAF@users.noreply.github.com>
2024-12-12 14:00:39 +0800

    Add Debug on Predict Image

--- services/predict.py
+++ services/predict.py
@@ -1,18 +1,15 @@
 from models.model import model
 from utils.preprocess import preprocess_image
 
-CLASSES = [
-    'Akar Busuk', 
-    'Busuk Batang', 
-    'Busuk Daun', 
-    'Hawar Daun dan Bunga', 
-    'Powdery Mildew', 
-    'Sehat'
-]
-
 def predict_image(image_stream):
+    classes = ['Akar Busuk', 'Busuk Batang', 'Busuk Daun', 'Hawar Daun dan Bunga', 'Powdery Mildew', 'Sehat']
+    print("Classes used in prediction:", classes)
+
     processed_image = preprocess_image(image_stream)
     predictions = model.predict(processed_image)
     probabilities = predictions[0]  
 
-    return {CLASSES[i]: float(probabilities[i] * 100) for i in range(len(CLASSES))}
+    result =  {classes[i]: float(probabilities[i] * 100) for i in range(len(classes))}
+    print("Prediction result:", result)
+
+    return result
